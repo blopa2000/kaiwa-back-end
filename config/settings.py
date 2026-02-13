8a1554be05b0c845b36b5171189a426d502d4652
blopa2000 <jpagudelo2000@gmail.com>
2026-02-13 15:13:41 -0500

    cors

--- config/settings.py
+++ config/settings.py
@@ -247,4 +247,7 @@ SIMPLE_JWT = {
 # ==============================
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
+    "http://127.0.0.1:4200",
+    "https://kaiwa-front-end.vercel.app",
+    "https://kaiwa-front-end-git-main-blopa2000s-projects.vercel.app",
 ]
